beb38287802bc5dc49827f08c9347deacfe60aed
Yazid Mekhtoub <ymekhtoub@gmail.com>
2026-02-24 02:18:34 +0100

    impr

--- frontend/src/app/pages/admin/admin-users/admin-users.component.ts
+++ frontend/src/app/pages/admin/admin-users/admin-users.component.ts
@@ -113,7 +113,7 @@ export class AdminUsersComponent extends BaseAdminListComponent implements OnIni
     });
   }
 
-  private handleLoadError(error: { status?: number }): void {
+  private handleLoadError(error: any): void {
     this.loading = false;
 
     if (error.status === 403) {
