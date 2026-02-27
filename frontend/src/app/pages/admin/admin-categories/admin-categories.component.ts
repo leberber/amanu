83e3c09cb6d739be155be1a933003ce347aa3576
Yazid Mekhtoub <ymekhtoub@gmail.com>
2026-02-27 15:12:17 +0100

    added the contaner

--- frontend/src/app/pages/admin/admin-categories/admin-categories.component.ts
+++ frontend/src/app/pages/admin/admin-categories/admin-categories.component.ts
@@ -7,6 +7,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { ADMIN_LIST_IMPORTS, ADMIN_DIALOG_IMPORTS } from '../../../shared/imports/admin-shared.imports';
 import { InlineEditState } from '../../../shared/utils/inline-edit-state';
 import { TableSkeletonComponent, SkeletonColumn } from '../../../shared/components/table-skeleton/table-skeleton.component';
+import { AgroclikPageContainerComponent } from '../../../shared/components/agroclik-page-container/agroclik-page-container.component';
 import { ROUTES, RouteHelpers } from '../../../core/constants/routes.constants';
 import { onLanguageChange } from '../../../core/utils/language-change.util';
 import { ProductService } from '../../../services/product.service';
@@ -22,7 +23,8 @@ import { BaseAdminListComponent, ColumnOption } from '../../../shared/base/base-
     ...ADMIN_LIST_IMPORTS,
     ...ADMIN_DIALOG_IMPORTS,
     PopoverModule,
-    TableSkeletonComponent
+    TableSkeletonComponent,
+    AgroclikPageContainerComponent
   ],
   providers: [ConfirmationService],
   templateUrl: './admin-categories.component.html',
